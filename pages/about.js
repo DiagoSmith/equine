183405467cee485976ea04f454688908953902ba
Roberto Floris <rmfloris@gmail.com>
2019-01-26 13:45:41 +0100

    Add copy for about, add formik, add initial work on price cards

--- pages/about.js
+++ pages/about.js
@@ -26,27 +26,27 @@ const About = () => (
     <StyledContainer>
       <TextContainer>
         <h1> About </h1>
-        <h3> Something Something horses something</h3>
+        <h3>
+          {" "}
+          Horses have been a huge part of my life since I was 5 years old.
+        </h3>
         <p>
-          Poop in the plant pot chase red laser dot or attack like a vicious
-          monster. Play time stare at guinea pigs so hiding behind the couch
-          until lured out by a feathery toy see owner, run in terror lick arm
-          hair, so try to jump onto window and fall while scratching at wall.
-          Yowling nonstop the whole night stand in doorway, unwilling to chose
-          whether to stay in or go out for murr i hate humans they are so
-          annoying but slap owner's face at 5am until human fills food dish
-          sleep in the bathroom sink. Meoooow kitten is playing with dead mouse
-          meow meow mama, chase imaginary bugs chew foot, and hopped up on
-          catnip. Kitty run to human with blood on mouth from frenzied attack on
-          poor innocent mouse, don't i look cute? meow to be let in for leave
-          dead animals as gifts, and knock over christmas tree i shredded your
-          linens for you. Kitten is playing with dead mouse. Eat too much then
-          proceed to regurgitate all over living room carpet while humans eat
-          dinner. Toy mouse squeak roll over hide from vacuum cleaner, for chase
-          the pig around the house so toy mouse squeak roll over. Kitty loves
-          pigs sniff other cat's butt and hang jaw half open thereafter, and
-          climb leg cats are cute and immediately regret falling into bathtub
-          claw drapes.{" "}
+          I started weekly riding lessons at my local riding school, and from
+          then my love for horses grew. I was 10 years old when my parents
+          bought my my first pony, whom I still own. Over the years, I have been
+          fortunate enough to have owned a variety of different horses. In my
+          younger years, I spent a lot of time show jumping at a local level. I
+          always knew I wanted to work with horses, so after school I decided to
+          study Equine Management at Bishop Burton College. It was during
+          college that we bought Basil, my 16.2hh Dutch Warmblood, who gave me a
+          new love for Dressage. Basil also took me round my first affiliated
+          show jumping course. After a few years of only being around my own
+          horses, I decided I wanted to work with horses again, as it had always
+          been my passion, and this is when I decided to look into Equine
+          Massage Therapy. I am now a fully qualified Equine Sports Massage
+          Therapist, and I aim to study Equine Physiotherapy in the near future
+          also, to expand my knowledge. There is nothing more rewarding to me,
+          than knowing I can help improve a horses well-being.
         </p>
       </TextContainer>
       <ImageContainer>
--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,57 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.section`
+  margin-left: 60px;
+  margin-right: 60px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  padding: 60px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const RateCardWrapper = styled.div`
+  padding: 40px;
+  display: flex;
+`;
+
+const RateCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  background: #f0f0f0;
+  width: 300px;
+  text-align: center;
+`;
+
+RateCard.Title = styled.h2``;
+
+RateCard.Price = styled.h1``;
+
+RateCard.Description = styled.p``;
+
 const Services = () => (
   <div>
-    <p> Services </p>
+    <StyledContainer>
+      <h1> Services </h1>
+
+      <RateCardWrapper>
+        <RateCard>
+          <RateCard.Title>Initial Consultation</RateCard.Title>
+          <RateCard.Price>£35</RateCard.Price>
+          <RateCard.Description>
+            Baseline heuristic tests for your horse
+          </RateCard.Description>
+        </RateCard>
+        <RateCard>
+          <RateCard.Title>Further Visits</RateCard.Title>
+          <RateCard.Price>£30</RateCard.Price>
+          <RateCard.Description>
+            Additional blah blah blah.
+          </RateCard.Description>
+        </RateCard>
+      </RateCardWrapper>
+    </StyledContainer>
   </div>
 );
 
